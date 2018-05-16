7846e7e99081f3e31da0566c616130bb9f9330f9
xiaoYu小宇 <songzy1989@163.com>
2018-05-16 18:54:22 +0800

    代码优化
    将card的id改为私有属性，card由class变为struct

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -57,16 +57,16 @@ class ViewController: UIViewController {
     }
     
     var emojiChoices = ["👻","🎃","👹","🤡","👿","👾","💩","👁","👨🏻‍✈️","🐼"]
-    var emoji = [Int:String]()
+    var emoji = [Card:String]()
     
     func emoji(for card:Card) -> String{
         
-        if  emoji[card.identifier] == nil, emojiChoices.count > 0{
+        if  emoji[card] == nil, emojiChoices.count > 0{
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            emoji[card] = emojiChoices.remove(at: randomIndex)
         }
         
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
 }
 
